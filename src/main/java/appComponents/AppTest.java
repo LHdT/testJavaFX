b25ff668dd5e90cf1e080b9ca71903582790a148
Luis Hidalgo <luishidalgodetena@gmail.com>
2018-01-21 10:12:04 -0600

    add ws definition classes and interfaces

--- src/main/java/appComponents/AppTest.java
+++ src/main/java/appComponents/AppTest.java
@@ -1,5 +1,19 @@
 package appComponents;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class AppTest {
-	//TODO lhdt: this is main class to show plugin functionality as simplify test
+
+	public static void main(String[] args) {
+		try {
+			URL url = new URL("http://www.webservicex.com/globalweather.asmx?wsdl");
+			
+		} catch (MalformedURLException e) {
+			System.out.println(e);
+			e.printStackTrace();
+		}
+
+	}
+
 }
